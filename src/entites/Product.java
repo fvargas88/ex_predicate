6d6e635e8fdd65ac9fbb3416b923f5d84af08b30
fvargas88 <fvargas_88@hotmail.com>
2023-03-24 11:42:25 -0300

    Reference method versão de implementação do predicado utiizando método estático

--- src/entites/Product.java
+++ src/entites/Product.java
@@ -21,6 +21,11 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
